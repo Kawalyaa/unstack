3d2a6314429b1ce0571fa40a2f8d02d72842105b
Kawalya Andrew <andrew58934@gmail.com>
2019-05-17 10:28:01 +0300

    adited db_con.py

--- app/db_con.py
+++ app/db_con.py
@@ -1,25 +1,34 @@
-# from contextlib import closing
+import os
 from flask import current_app
 import psycopg2
+from psycopg2.extras import RealDictCursor
 # from app.tables import tables
 
 
 class DataBaseConnection:
     """ Handles the main connection to the database of the app setting """
 
+    # def init_db(self):
+    # """Create tables and return connection"""
+    # con = psycopg2.connect("dbname='unstack' host='localhost' port=5432  user='kawalya' password='kawalyaa'")
+    # with con as con, con.cursor() as cur:
+    # with current_app.open_resource('schema.sql', mode='r') as sql:
+    # cur.execute(sql.read())
+    # con.commit()
+    # return con
+
     def init_db(self):
-        """Create tables and return connection"""
-        con = psycopg2.connect("dbname='unstack' host='localhost' port=5432  user='kawalya' password='kawalyaa'")
-        # cur = con.cursor()
-        with con as con, con.cursor() as cur:
+        # con = psycopg2.connect('')
+        con = psycopg2.connect("dbname='unstack' host='127.0.0.1' port=5432  user='kawalya' password='kawalyaa'")
+        with con as con, con.cursor(cursor_factory=RealDictCursor) as cur:
             with current_app.open_resource('schema.sql', mode='r') as sql:
                 cur.execute(sql.read())
                 con.commit()
-        return con
+                return con
 
     def destroydb(self):
         """Deletes all tables after tests have been run"""
-        con = psycopg2.connect("dbname='unstack' host='localhost' port=5432  user='kawalya' password='kawalyaa'")
+        con = psycopg2.connect("dbname='unstack' host='127.0.0.1' port=5432  user='kawalya' password='kawalyaa'")
         cur = con.cursor()
         users = """DROP TABLE IF EXISTS users CASCADE;"""
         blacklist = """DROP TABLE IF EXISTS blacklist CASCADE;"""
