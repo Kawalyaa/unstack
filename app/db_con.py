90693c131ca69254c8b70e0d5741599d9629122c
Kawalya Andrew <andrew58934@gmail.com>
2019-05-18 07:59:48 +0300

    edited the db_con file

--- app/db_con.py
+++ app/db_con.py
@@ -1,7 +1,7 @@
 # import os
 from flask import current_app
 import psycopg2
-from psycopg2.extras import RealDictCursor
+# from psycopg2.extras import RealDictCursor
 # from app.tables import tables
 
 
@@ -16,20 +16,32 @@ class DataBaseConnection:
     # cur.execute(sql.read())
     # con.commit()
     # return con
-
     def init_db(self):
-        con = psycopg2.connect('')
+        db_url = current_app.config['DATABASE_URL']
+        con = psycopg2.connect(db_url)
         # con = psycopg2.connect("dbname='unstack' port=5432  user='kawalya' password='kawalyaa'")
-        with con as con, con.cursor(cursor_factory=RealDictCursor) as cur:
+        with con as con, con.cursor() as cur:
             with current_app.open_resource('schema.sql', mode='r') as sql:
                 cur.execute(sql.read())
-                con.commit()
-                return con
+        con.commit()
+        return con
+
+    # def _init_db(self):
+    #    db_url = current_app.config['DATABASE_URL']
+    #    con = psycopg2.connect(db_url)
+    # con = psycopg2.connect("dbname='unstack_test' port=5432  user='kawalya' password='kawalyaa'")
+    #    with con as con, con.cursor(cursor_factory=RealDictCursor) as cur:
+    #        with current_app.open_resource('schema.sql', mode='r') as sql:
+    #            cur.executcon.commit()
+    #    return ce(sql.read())con.commit()
+    #    return c
+    #    con.commit()
+    #    return con
 
     def destroydb(self):
         """Deletes all tables after tests have been run"""
-        con = psycopg2.connect('')
-        # con = psycopg2.connect("dbname='unstack' host='127.0.0.1' port=5432  user='kawalya' password='kawalyaa'")
+        # con = psycopg2.connect('')
+        con = psycopg2.connect("dbname='unstack' host='127.0.0.1' port=5432  user='kawalya' password='kawalyaa'")
         cur = con.cursor()
         users = """DROP TABLE IF EXISTS users CASCADE;"""
         blacklist = """DROP TABLE IF EXISTS blacklist CASCADE;"""
